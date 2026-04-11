fbce24f02941688180a77fa48d8764afe87db8a9
Henry Proctor <henry.rt.proctor@gmail.com>
2026-04-11 23:21:40 +0100

    added tailwind to the nav bar.

--- client/Src/components/Navbar.tsx
+++ client/Src/components/Navbar.tsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 export default function Navbar(){
 
     return(
-        <nav>
-            <NavLink to='/'> Home</NavLink>
-            <NavLink to='/climbs'> Climbs</NavLink>
-            <NavLink to='/log'> LogBook</NavLink>
-            <NavLink to='/profile'>Profile </NavLink>
+    <nav className="flex justify-evenly bg-emerald-900 w-full py-[2vh]">
+            <NavLink className=' rounded hover:bg-gray-700/50 py-[3vh] px-[5vw] text-gray-100' to='/'> Home</NavLink>
+            <NavLink className=' rounded hover:bg-gray-700/50 py-[3vh] px-[5vw] text-gray-100' to='/climbs'> Climbs</NavLink>
+            <NavLink className=' rounded hover:bg-gray-700/50 py-[3vh] px-[5vw] text-gray-100' to='/log'> LogBook</NavLink>
+            <NavLink className=' rounded hover:bg-gray-700/50 py-[3vh] px-[5vw] text-gray-100' to='/profile'>Profile </NavLink>
         </nav>
     )
 }
